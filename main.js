1128940f955aff1aad9b976ebe3fb7732115a2fe
Joao Vitor Cezario Sanches <joaovitormanolo@gmail.com>
2022-02-04 16:23:36 -0300

    Implemented basic setTime function

--- main.js
+++ main.js
@@ -3,3 +3,11 @@ const addTime = (pointer) => {
     current = parseInt(current.slice(0, current.length - 3)) + 10
     document.documentElement.style.setProperty(`--${pointer}`, `${current}deg`)
 }
+
+const setTime = (pointer) => {
+    let current = new Date().getHours()
+    current = current%12 * 360/12
+    document.documentElement.style.setProperty(`--${pointer}`, `${current}deg`)
+}
+
+setTime('hour')
