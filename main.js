178329adbb2f0a24630055edaf39d8df7200d31b
Joao Vitor Cezario Sanches <joaovitormanolo@gmail.com>
2022-02-05 18:18:14 -0300

    Fixed bugs before dinamically generate content

--- main.js
+++ main.js
@@ -1,23 +1,35 @@
+// const data = {
+//     hour: 10**3
+// }
+
 const data = {
-    hour: 3.6 * 10**6
+    second: {
+        value: 10**3,
+        completeLoop: 60
+    },
+    hour: {
+        value: 3.6 * 10 ** 6,
+        completeLoop: 12
+    }
 }
-const clockTotal = 12 * 3.6 * 10**6
 const GMT = -3
 
-const addTime = (pointer) => {
-    let current = getComputedStyle(document.documentElement).getPropertyValue(`--${pointer}`) || '0deg'
-    current = parseInt(current.slice(0, current.length - 3)) + (data[pointer]*360/clockTotal)
-    document.documentElement.style.setProperty(`--${pointer}`, `${current}deg`)
-}
+// const addTime = (pointer) => {
+//     let current = getComputedStyle(document.documentElement).getPropertyValue(`--${pointer}`) || '0deg'
+//     current = parseInt(current.slice(0, current.length - 3)) + (data[pointer]*360/clockTotal)
+//     document.documentElement.style.setProperty(`--${pointer}`, `${current}deg`)
+// }
 
 const setTime = (pointer) => {
     let current = Date.now()
-    let ratio = data[pointer]/clockTotal
 
-    current = (360*ratio) * (((current%clockTotal)/data[pointer]) + GMT)
+    let correction = pointer === 'hour' ? GMT : 0
+
+    current = (360/data[pointer].completeLoop) * (((current%(data[pointer].completeLoop*data[pointer].value)/data[pointer].value)) + correction)
 
     document.documentElement.style.setProperty(`--${pointer}`, `${current}deg`)
 }
 
 setTime('hour')
-// setInterval(addTime, 1000, 'hour')
+setInterval(setTime, 1000, 'hour')
+setInterval(setTime, 1000, 'second')
