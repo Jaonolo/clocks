68427aa6a4c0c352d8e9d0c5e53fe6ed9d6384c4
Joao Vitor Cezario Sanches <joaovitormanolo@gmail.com>
2022-02-05 16:54:27 -0300

    Fully working setTime function

--- main.js
+++ main.js
@@ -2,16 +2,20 @@ const data = {
     hour: 3.6 * 10**6
 }
 const clockTotal = 12 * 3.6 * 10**6
+const GMT = -3
 
 const addTime = (pointer) => {
-    let current = getComputedStyle(document.documentElement).getPropertyValue(`--${pointer}`)
-    current = parseInt(current.slice(0, current.length - 3)) + (data['hour']*360/clockTotal)
+    let current = getComputedStyle(document.documentElement).getPropertyValue(`--${pointer}`) || '0deg'
+    current = parseInt(current.slice(0, current.length - 3)) + (data[pointer]*360/clockTotal)
     document.documentElement.style.setProperty(`--${pointer}`, `${current}deg`)
 }
 
 const setTime = (pointer) => {
-    let current = new Date().getHours()
-    current = current%12 * 360/12
+    let current = Date.now()
+    let ratio = data[pointer]/clockTotal
+
+    current = (360*ratio) * (((current%clockTotal)/data[pointer]) + GMT)
+
     document.documentElement.style.setProperty(`--${pointer}`, `${current}deg`)
 }
 
